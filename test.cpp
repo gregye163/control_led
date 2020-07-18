66bef982205b8b8bc5247af922d8535559ccc885
gregye163 <gregye@163.com>
2020-07-18 14:57:30 +0800

    [add] 新增控制灯亮，一秒亮一次

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <wiringPi.h>
+
+#define GPIO 7
+
+int main(){
+    wiringPiSetupSys();
+    pinMode(GPIO, OUTPUT);
+    // pinMode(GPIO, INPUT);
+    while(1){
+        digitalWrite(GPIO, HIGH);
+        sleep(1);
+        digitalWrite(GPIO, LOW);
+        sleep(1);
+    }
+    wiringPiTerminateSys();
+    return 0 ;
+}
+
+
+// 编译:  g++ test.cpp -lwiringPi -lpthread -o test
+
+// 安装wiringPi 1.0：
+// sudo dnf install wiringPi-1.0_EAIDK-1.openailab.fc28.aarch64
