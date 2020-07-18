d0c0e9503074b6d9dbdfd9042c952627e3c5e64e
gregye163 <gregye@163.com>
2020-07-18 15:08:41 +0800

    [modify] 增加安装库功能

--- test.cpp
+++ test.cpp
@@ -21,4 +21,4 @@ int main(){
 // 编译:  g++ test.cpp -lwiringPi -lpthread -o test
 
 // 安装wiringPi 1.0：
-// sudo dnf install wiringPi-1.0_EAIDK-1.openailab.fc28.aarch64
+// sudo dnf install wiringPi-1.0_EAIDK-1.openailab.fc28.aarch64 或者 sudo yum install wiringPi
